b348a86bdde032497a4c39f0f8774981dbc0a766
Not <notactualdev@gmail.com>
2022-06-02 14:53:03 +0200

    Created VisualStudio project

---
+++ Assessment - String Concatenation/Assessment - String Concatenation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment___String_Concatenation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
